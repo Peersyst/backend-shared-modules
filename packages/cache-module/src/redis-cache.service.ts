c590fcaf00866511c8873be9cdebc1440d2b9ad7
Sergi <sergi.cg8@gmail.com>
2021-11-17 10:51:35 +0100

    updated http call to callback function

--- packages/cache-module/src/redis-cache.service.ts
+++ packages/cache-module/src/redis-cache.service.ts
@@ -1,26 +1,21 @@
-import {CACHE_MANAGER, HttpService, Inject, Injectable} from "@nestjs/common";
-import {Cache, CachingConfig} from "cache-manager";
+import {CACHE_MANAGER, Inject, Injectable} from "@nestjs/common";
+import {Cache} from "cache-manager";
 
 @Injectable()
 export class RedisCacheService {
-    constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache, @Inject(HttpService) private readonly http: HttpService) {}
+    constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 
-    public async cacheGet<T>(callId: string, url: string, headers: { [key: string]: string }, ttl?: number): Promise<T> {
+    public async cacheGet<T>(callId: string, callback: () => Promise<T>, ttl?: number): Promise<T> {
         const cached = await this.cacheManager.get<T>(callId);
         if (cached) {
             return cached;
         }
 
-        const response = await this.http.get(url, {
-            headers,
-        }).toPromise();
-        if (response.status < 300) {
-            await this.cacheManager.set(callId, response.data, {
-                ttl: ttl || 60*5, // default ttl 5 min
-            });
-            return response.data;
-        } else {
-            throw new Error(`Error in Get request ${callId}`);
-        }
+        const response = await callback();
+
+        await this.cacheManager.set(callId, response, {
+            ttl: ttl || 60*5, // default ttl 5 min
+        });
+        return response;
     }
 }
