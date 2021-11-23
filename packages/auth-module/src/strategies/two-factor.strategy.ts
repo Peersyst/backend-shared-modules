4a2b500d2f2bb99aff70fa39742d28eb17580dce
Sergi <sergi.cg8@gmail.com>
2021-11-23 11:39:49 +0100

    added two factor strategy

---
+++ packages/auth-module/src/strategies/two-factor.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { ConfigService } from "@nestjs/config";
+import { JwtPayloadDTOI } from "../dto/jwt-payload.dto";
+
+@Injectable()
+export class TwoFactorStrategy extends PassportStrategy(Strategy, "two-factor") {
+    constructor(configService: ConfigService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: configService.get("server.secretKey"),
+        });
+    }
+
+    async validate(payload: JwtPayloadDTOI): Promise<{ email: string; id: number; type: string }> {
+        if (payload.needs2fa && !payload.isTwoFactorAuthenticated) {
+            throw new Error("Missing 2fa login");
+        }
+        return { email: payload.email, id: payload.id, type: payload.type };
+    }
+}
