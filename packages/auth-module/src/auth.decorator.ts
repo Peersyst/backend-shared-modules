20079afb8a382d9892f81e35721731b06ceeb957
Joan Grau <jgrau@peersyst.com>
2021-11-23 01:12:15 +0100

    add JwtAuthGuard to decorator to sign jwt and add user

--- packages/auth-module/src/auth.decorator.ts
+++ packages/auth-module/src/auth.decorator.ts
@@ -1,14 +1,14 @@
-import { applyDecorators, ForbiddenException, SetMetadata, UnauthorizedException, UseGuards } from "@nestjs/common";
+import { applyDecorators, ForbiddenException, SetMetadata, UnauthorizedException, UseGuards, Logger } from "@nestjs/common";
 import { ApiBearerAuth } from "@nestjs/swagger";
 import { ApiException } from "@nanogiants/nestjs-swagger-api-exception-decorator";
 import { RolesGuard } from "./guards/roles.guard";
 import { WithUserType } from "./entities/AuthUser";
-// import { TwoFactorAuthGuard } from "./guards/two-factor-auth.guard";
+import { JwtAuthGuard } from "./guards/jwt-auth.guard";
 
 export function Authenticated<T>(role?: WithUserType<T> | string): MethodDecorator {
     return applyDecorators(
         SetMetadata("role", role),
-        // UseGuards(TwoFactorAuthGuard),
+        UseGuards(JwtAuthGuard),
         ApiBearerAuth(),
         UseGuards(RolesGuard),
         ApiException(() => UnauthorizedException),
