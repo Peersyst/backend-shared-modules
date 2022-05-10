7f18d0ee2dccbeb10c3f8367b10a26406775cf12
Joan Grau <jgrau@peersyst.com>
2022-05-10 10:49:53 +0200

    interceptor sets status instead of throwing error

--- packages/unleash-module/src/unleash.interceptor.ts
+++ packages/unleash-module/src/unleash.interceptor.ts
@@ -2,13 +2,21 @@ import { NestInterceptor, Injectable, Inject, ExecutionContext, HttpStatus, Call
 import { Observable } from "rxjs";
 import { UnleashBusinessException } from "./exception/business.exception";
 import { UnleashErrorCode } from "./exception/error-codes";
+import { Response } from "express";
 
 @Injectable()
 export class UnleashInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
         const req = context.switchToHttp().getRequest();
         if (req.toggled === false) {
-            throw new UnleashBusinessException(UnleashErrorCode.METHOD_NOT_AVAILABLE);
+            const ctx = context.switchToHttp();
+            const response = ctx.getResponse<Response>();
+            response.status(HttpStatus.METHOD_NOT_ALLOWED).json({
+                statusCode: HttpStatus.METHOD_NOT_ALLOWED,
+                message: "METHOD_NOT_ALLOWED",
+            });
+
+            return null;
         }
         
         return next.handle();
