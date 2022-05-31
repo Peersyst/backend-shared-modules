73384f8b95854765bc6b8f2351eaa1e6946716b2
Sergi <sergi.cg8@gmail.com>
2022-05-31 12:52:54 +0200

    version

--- packages/ipfs-module/src/ipfs.service.ts
+++ packages/ipfs-module/src/ipfs.service.ts
@@ -4,23 +4,23 @@ import * as IPFS from "ipfs";
 @Injectable()
 export class IpfsService {
     private ipfsNode: any;
-    private createPromise: Promise<any>;
+    private connection: Promise<void>;
 
     constructor() {
-        this.createPromise = IPFS.create().then(async (node) => {
+        this.connection = IPFS.create().then(async (node) => {
             this.ipfsNode = node;
         });
     }
 
     async uploadFile(fileBuffer: Buffer): Promise<string> {
-        await this.createPromise;
+        await this.connection;
         const file = await this.ipfsNode.add(fileBuffer);
         console.log(`Added file ${file.cid}`);
         return file.cid.toString();
     }
 
     async getFile(cid: string): Promise<Buffer> {
-        await this.createPromise;
+        await this.connection;
         const chunks = [];
         for await (const chunk of this.ipfsNode.cat(cid)) {
             chunks.push(chunk);
