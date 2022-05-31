921b7a504e151ea16fe9d06cd154ccec373254ed
Sergi <sergi.cg8@gmail.com>
2022-05-31 12:35:43 +0200

    added await connection promise to ipfs module

--- packages/ipfs-module/src/ipfs.service.ts
+++ packages/ipfs-module/src/ipfs.service.ts
@@ -4,20 +4,23 @@ import * as IPFS from "ipfs";
 @Injectable()
 export class IpfsService {
     private ipfsNode: any;
+    private createPromise: Promise<any>;
 
     constructor() {
-        IPFS.create().then(async (node) => {
+        this.createPromise = IPFS.create().then(async (node) => {
             this.ipfsNode = node;
         });
     }
 
     async uploadFile(fileBuffer: Buffer): Promise<string> {
+        await this.createPromise;
         const file = await this.ipfsNode.add(fileBuffer);
         console.log(`Added file ${file.cid}`);
         return file.cid.toString();
     }
 
     async getFile(cid: string): Promise<Buffer> {
+        await this.createPromise;
         const chunks = [];
         for await (const chunk of this.ipfsNode.cat(cid)) {
             chunks.push(chunk);
