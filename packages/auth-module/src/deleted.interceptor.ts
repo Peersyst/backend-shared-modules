e47836c5fcab16f3583d4898c923cee8bda8968f
AgustinMJ <agustinmillanjimenez+github@gmail.com>
2023-06-09 11:24:44 +0200

    Wake up Jenkins

--- packages/auth-module/src/deleted.interceptor.ts
+++ packages/auth-module/src/deleted.interceptor.ts
@@ -13,7 +13,6 @@ export class DeletedInterceptor implements NestInterceptor {
                 statusCode: HttpStatus.UNAUTHORIZED,
                 message: "Unauthorized",
             });
-
             return null;
         }
 
